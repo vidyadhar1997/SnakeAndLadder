bdedf83d18162e8f3c0c6644b02d93178dea03d0
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-01 19:03:37 +0530

    [vidyadhar hudge]Add:intialize the poistion of players

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -4,9 +4,13 @@ namespace SnakeAndLadder
 {
     class Program
     {
+        const int START_POISTION = 0;
+        const int END_POSTION = 100;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to snake and ladder program");
+            Console.WriteLine("start postion is: "+START_POISTION);
+            Console.WriteLine("end postion is: " + END_POSTION);
         }
     }
 }
