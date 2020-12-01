b1a59b4c784b55a7003ddfa02ee60587add0702c
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-01 19:15:44 +0530

    [vidyadhar hudge]Add:Roll dice to get current poistion

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -6,9 +6,19 @@ namespace SnakeAndLadder
     {
         const int START_POISTION = 0;
         const int END_POSTION = 100;
+        public static int currentPoistion = 0;
+
+        public static int throwDice()
+        {
+            Random random = new Random();
+            int number = random.Next(1, 7);
+            return number;
+        }
 
         static void Main(string[] args)
         {
+            currentPoistion += throwDice();
+            Console.WriteLine("Current poistion is=" + currentPoistion);
             Console.WriteLine("start postion is: "+START_POISTION);
             Console.WriteLine("end postion is: " + END_POSTION);
         }
