bb3a8aa4042208466f8ca3b8df3f7c8c372c3841
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-01 20:27:58 +0530

    [vidyadhar hudge]Add:Check for option ie play,no play,ladder or snake

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -7,20 +7,47 @@ namespace SnakeAndLadder
         const int START_POISTION = 0;
         const int END_POSTION = 100;
         public static int currentPoistion = 0;
+        public static int selectdOption = 0;
+        static Random random = new Random();
 
         public static int throwDice()
         {
-            Random random = new Random();
-            int number = random.Next(1, 7);
-            return number;
+            return random.Next(1, 7);
+        }
+
+        public static int checkForOption()
+        {
+           return random.Next(1, 4);
+        }
+
+        public static int playerOption()
+        {
+            int throwDices = throwDice();
+            int selectdOption = checkForOption();
+            if (selectdOption == 2)
+            {
+                currentPoistion += throwDices;
+            }
+            else if (selectdOption == 3)
+            {
+                currentPoistion -= throwDices;
+                if (currentPoistion < 0)
+                {
+                    currentPoistion = START_POISTION;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Do nothing player stay in same poistion");
+            }
+            return currentPoistion;
         }
 
         static void Main(string[] args)
         {
-            currentPoistion += throwDice();
-            Console.WriteLine("Current poistion is=" + currentPoistion);
             Console.WriteLine("start postion is: "+START_POISTION);
             Console.WriteLine("end postion is: " + END_POSTION);
+            Console.WriteLine("current poistion is= " + playerOption());
         }
     }
 }
